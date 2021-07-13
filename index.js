1a3ef5ce7ef0dcab47ac3d5a44adb1e8ee5b9fbe
Laura <lnkeat@gmail.com>
2021-07-12 21:30:58 -0600

    commit tree a72a6cc2f87bb0a8b5912a7e89f079255ba0b000

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(arr, string){
+    return arr.filter(function (name){
+      if(name.toLowerCase() === string.toLowerCase()){return name};
+    })
+}
+
+function fuzzyMatch(arr, string){
+    let lowerString = string.toLowerCase();
+    return arr.filter(function (name){
+      if (name.toLowerCase().slice(0,2) === lowerString.slice(0,2)){return name}
+    })
+}
+
+function matchName(obj, string){
+    return obj.filter(function (item){
+      if(item.name === string){return item}
+    })
+  }
